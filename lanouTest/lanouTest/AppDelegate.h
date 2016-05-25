178faec07f56dfc52a450cc4e32dc7be561ff333
小林子 <836188708@qq.com>
2016-05-25 15:50:52 +0800

    新建的一个测试工程

---
+++ lanouTest/lanouTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lanouTest
+//
+//  Created by 林子还 on 16/5/25.
+//  Copyright © 2016年 林子还. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lanouTest/lanouTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lanouTest
+//
+//  Created by 林子还 on 16/5/25.
+//  Copyright © 2016年 林子还. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
